61e64f4a74bebce4722b93f1cb93d4f8ed129f1e
Harry <bansmp717_4@hotmail.com>
2026-05-31 06:29:48 -0300

    JHM-6 #comment Configurada arquitectura de variables de entorno .env para despliegue cloud

--- jhm-pneumonia-cnn/api/database.py
+++ jhm-pneumonia-cnn/api/database.py
@@ -2,11 +2,14 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
+# 🧠 Intenta leer la URL del archivo .env. Si por alguna razón no la encuentra,
+# usa por defecto ("fallback") la ruta local de Docker para que nunca falle en tu casa.
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "postgresql+psycopg2://jhm_user:jhm_password@localhost:5433/jhm_pneumonia_auditoria"
+    "postgresql+psycopg2://jhm_user:jhm_password@db:5432/jhm_pneumonia_auditoria"
 )
 
+# pool_pre_ping=True verifica que la conexión con Postgres esté viva antes de hacer consultas
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
